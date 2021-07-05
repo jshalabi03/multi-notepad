dfbb2de1aabdcd06ff288568117e9a3646fb97e0
Jacob Shalabi <jshalabi03@gmail.com>
2021-07-05 14:53:51 -0500

    Initialzied stage with example label

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application{
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Multi Notepad");
+
+        Label welcome = new Label();
+        welcome.textProperty().set("Welcome!");
+
+        StackPane root = new StackPane();
+        root.getChildren().add(welcome);
+
+        Scene scene = new Scene(root, 300, 200);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
     }
 }
