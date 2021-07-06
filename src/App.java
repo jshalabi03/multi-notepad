821ccde25a1d5d3af1a819de78602ec9c871b1ae
Jacob Shalabi <jshalabi03@gmail.com>
2021-07-06 12:02:16 -0500

    initialized fxml loader in app

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
@@ -7,16 +9,14 @@ import javafx.stage.Stage;
 public class App extends Application{
 
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Multi Notepad");
+    public void start(Stage primaryStage) throws Exception{
+        
 
-        Label welcome = new Label();
-        welcome.textProperty().set("Welcome!");
-
-        StackPane root = new StackPane();
-        root.getChildren().add(welcome);
+        Parent root = FXMLLoader.load(getClass().getResource("ui.fxml"));
 
         Scene scene = new Scene(root, 300, 200);
+
+        primaryStage.setTitle("Multi Notepad");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
