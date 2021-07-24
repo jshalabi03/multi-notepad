9206b678ff917bbfd2334d52c85c529ed5d5b41e
Jacob Shalabi <jshalabi03@gmail.com>
2021-07-24 12:57:42 -0500

    created controllers for each UI and extended scene controller for each

---
+++ src/UI_6_controller.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import javafx.scene.Node;
+
+public class UI_6_controller extends SceneController {
+    
+}
