378c04a7ffc57890c248200f902f9539c6d796cd
Jacob Shalabi <jshalabi03@gmail.com>
2021-07-24 09:50:29 -0500

    refactored scene switching event handlers

--- src/SceneController.java
+++ src/SceneController.java
@@ -1,7 +1,4 @@
-
-
 import java.io.IOException;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -25,43 +22,31 @@ public class SceneController {
 
     @FXML
     public void switch_ui_1(ActionEvent e) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("ui_1.fxml"));
-        this.stage = (Stage)this.currentMenuBar.getScene().getWindow();
-        this.scene = new Scene(root, App.V_RES, App.H_RES - App.TASKBAR_HEIGHT - App.MENU_HEADER_HEIGHT);
-        this.stage.setMaximized(true);
-        this.stage.setScene(this.scene);
+        switchUI("ui_1.fxml");
     }
 
     @FXML
     public void switch_ui_2V(ActionEvent e) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("ui_2V.fxml")); 
-        this.stage = (Stage)this.currentMenuBar.getScene().getWindow();
-        this.scene = new Scene(root, App.V_RES, App.H_RES - App.TASKBAR_HEIGHT - App.MENU_HEADER_HEIGHT);
-        this.stage.setMaximized(true);
-        this.stage.setScene(this.scene);
+        switchUI("ui_2V.fxml");
     }
 
     @FXML
     public void switch_ui_2H(ActionEvent e) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("ui_2H.fxml"));
-        this.stage = (Stage)this.currentMenuBar.getScene().getWindow();
-        this.scene = new Scene(root, App.V_RES, App.H_RES - App.TASKBAR_HEIGHT - App.MENU_HEADER_HEIGHT);
-        this.stage.setMaximized(true);
-        this.stage.setScene(this.scene);
+        switchUI("ui_2H.fxml");
     }
     
     @FXML
     public void switch_ui_4(ActionEvent e) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("ui_4.fxml"));
-        this.stage = (Stage)this.currentMenuBar.getScene().getWindow();
-        this.scene = new Scene(root, App.V_RES, App.H_RES - App.TASKBAR_HEIGHT - App.MENU_HEADER_HEIGHT);
-        this.stage.setMaximized(true);
-        this.stage.setScene(this.scene);
+        switchUI("ui_4.fxml");
     }
 
     @FXML
     public void switch_ui_6(ActionEvent e) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("ui_6.fxml"));
+        switchUI("ui_6.fxml");
+    }
+
+    private void switchUI(String url) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource(url));
         this.stage = (Stage)this.currentMenuBar.getScene().getWindow();
         this.scene = new Scene(root, App.V_RES, App.H_RES - App.TASKBAR_HEIGHT - App.MENU_HEADER_HEIGHT);
         this.stage.setMaximized(true);
